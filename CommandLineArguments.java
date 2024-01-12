a572e5507f124fc78e205358d16838422619f19c
ayyappa9866 <ayyappa9866@gmail.com>
2024-01-12 16:29:49 +0530

    CommandLine Arguments Using Loop final

--- CommandLineArguments.java
+++
@@ -1,8 +0,0 @@
-public class CommandLineArguments {
-	public static void main(String[] args) {
-		double num1 = Double.parseDouble(args[0]);
-		double num2 = Double.parseDouble(args[1]);
-		double sum = num1 + num2;
-		System.out.println("Sum is : "+sum);
-	}
-}
